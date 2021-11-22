093c5aa129bec10acb7a52da22f83f22fc2c83b0
Pieter Nijs <pieternijs@live.be>
2021-11-22 14:08:30 +0100

    Repro

---
+++ src/ReproRandomNumberGeneratorCrash/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using System;
+using System.Security.Cryptography;
+
+namespace ReproRandomNumberGeneratorCrash
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            try
+            {
+                var x = RandomNumberGenerator.GetInt32(10);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.GetType().Name);
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                System.Diagnostics.Debug.WriteLine(ex.StackTrace);
+                throw;
+            }
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            CounterLabel.Text = $"Current count: {count}";
+
+            SemanticScreenReader.Announce(CounterLabel.Text);
+        }
+    }
+}
